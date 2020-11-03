9978c1ea9e9460388d5eccbd8579bdcc30007270
dikyindrah <dikyindrah@gmail.com>
2020-11-03 20:34:27 +0700

    Mengubah nama Main.py menjadi Script.py pada direktori 01-Apa Itu Python dan menambahkan sejarah python

---
+++ 01-Apa Itu Python/Script.py
@@ -0,0 +1,49 @@
+'''
+Sejarah Python
+
+Python diciptakan oleh Guido van Rossum pertama kali di Centrum Wiskunde & Informatica (CWI) di Belanda 
+pada awal tahun 1990-an. Bahasa python terinspirasi dari bahasa pemrograman ABC. Sampai sekarang, Guido masih 
+menjadi penulis utama untuk python, meskipun bersifat open source sehingga ribuan orang juga berkontribusi 
+dalam mengembangkannya.
+
+Di tahun 1995, Guido melanjutkan pembuatan python di Corporation for National Research Initiative (CNRI) 
+di Virginia Amerika, di mana dia merilis beberapa versi dari python.
+
+Pada Mei 2000, Guido dan tim Python pindah ke BeOpen.com dan membentuk tim BeOpen PythonLabs. Di bulan Oktober 
+pada tahun yang sama, tim python pindah ke Digital Creation (sekarang menjadi Perusahaan Zope). Pada tahun 2001, 
+dibentuklah Organisasi Python yaitu Python Software Foundation (PSF). PSF merupakan organisasi nirlaba yang dibuat 
+khusus untuk semua hal yang berkaitan dengan hak intelektual Python. Perusahaan Zope menjadi anggota sponsor dari PSF.
+
+Nama python sendiri tidak berasal dari nama ular yang kita kenal. Guido adalah penggemar grup komedi Inggris 
+bernama Monty Python. Ia kemudian menamai bahasa ciptaannya dengan nama Python.
+
+Semua versi python yang dirilis bersifat open source. Dalam sejarahnya, hampir semua rilis python menggunakan 
+lisensi GFL-compatible. Berikut adalah versi mayor dan minor python berikut tanggal rilisnya.
+
+Python 1.0 – Januari 1994
+    Python 1.2 – 10 April 1995
+    Python 1.3 – 12 Oktober 1995
+    Python 1.4 – 25 Oktober 1996
+    Python 1.5 – 31 Desember 1997
+    Python 1.6 – 5 September 2000
+Python 2.0 – 16 Oktober 2000
+    Python 2.1 – 17 April 2001
+    Python 2.2 – 21 Desember 2001
+    Python 2.3 – 29 Juli 2003
+    Python 2.4 – 30 Nopember 2004
+    Python 2.5 – 19 September 2006
+    Python 2.6 – 1 Oktober 2008
+    Python 2.7 – 3 Juli 2010
+Python 3.0 – 3 Desember 2008
+    Python 3.1 – 27 Juni 2009
+    Python 3.2 – 20 Februari 2011
+    Python 3.3 – 29 September 2012
+    Python 3.4 – 16 Maret 2014
+    Python 3.5 – 13 September 2015
+    Python 3.6 – 23 Desember 2016
+    Python 3.7 – 27 Juni 2018
+    Python 3.8 – 14 Oktober 2019
+    Python 3.9 – 5 Oktober 2020
+
+Sumber : https://www.pythonindo.com/
+'''
