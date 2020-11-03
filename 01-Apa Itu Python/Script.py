66f70505ab5f284adc3a48d97ebad42aabee1a0d
dikyindrah <dikyindrah@gmail.com>
2020-11-03 21:40:53 +0700

    Menambahkan pengertian pyhton dan kenama memilih python pada file scrip di

--- 01-Apa Itu Python/Script.py
+++ 01-Apa Itu Python/Script.py
@@ -1,5 +1,24 @@
 '''
-Sejarah Python
+PENGERTIAN BAHASA PEMROGRAMAN PYTHON
+
+Python adalah bahasa pemrograman tingkat tinggi yang ditafsirkan, berorientasi objek, dengan semantik dinamis. 
+Struktur data bawaan tingkat tinggi, dikombinasikan dengan pengetikan dinamis dan pengikatan dinamis, membuatnya 
+sangat menarik untuk Pengembangan Aplikasi Cepat, serta untuk digunakan sebagai bahasa skrip atau perekat untuk 
+menghubungkan komponen yang ada bersama-sama. Sintaks Python yang sederhana dan mudah dipelajari menekankan pada 
+keterbacaan dan karena itu mengurangi biaya pemeliharaan program. Python mendukung modul dan paket, yang mendorong 
+modularitas program dan penggunaan kembali kode. Penerjemah Python dan pustaka standar yang luas tersedia dalam bentuk 
+sumber atau biner tanpa biaya untuk semua platform utama, dan dapat didistribusikan secara bebas.
+
+Seringkali, programmer jatuh cinta dengan Python karena peningkatan produktivitas yang disediakannya. Karena tidak ada 
+langkah kompilasi, siklus edit-test-debug sangat cepat. Men-debug program Python itu mudah: bug atau masukan yang buruk 
+tidak akan pernah menyebabkan kesalahan segmentasi. Sebaliknya, ketika interpreter menemukan kesalahan, itu memunculkan 
+pengecualian. Ketika program tidak menangkap pengecualian, penerjemah mencetak jejak tumpukan. Debugger level sumber memungkinkan 
+pemeriksaan variabel lokal dan global, evaluasi ekspresi arbitrer, menyetel breakpoint, menelusuri kode baris pada satu waktu, 
+dan seterusnya. Debugger ditulis dengan Python itu sendiri, bersaksi tentang kekuatan introspektif Python. Di sisi lain, seringkali 
+cara tercepat untuk men-debug program adalah dengan menambahkan beberapa pernyataan cetak ke sumbernya: siklus edit-test-debug yang 
+cepat membuat pendekatan sederhana ini sangat efektif.
+
+SEJARAH PYTHON
 
 Python diciptakan oleh Guido van Rossum pertama kali di Centrum Wiskunde & Informatica (CWI) di Belanda 
 pada awal tahun 1990-an. Bahasa python terinspirasi dari bahasa pemrograman ABC. Sampai sekarang, Guido masih 
@@ -44,6 +63,54 @@ Python 3.0 – 3 Desember 2008
     Python 3.7 – 27 Juni 2018
     Python 3.8 – 14 Oktober 2019
     Python 3.9 – 5 Oktober 2020
+
+MENGAPA HARUS PYTHON?
+
+Mengapa harus Python? Bukankah masih banyak bahasa pemrograman lain di luar sana? 
+Apa kelebihan Python?
+
+Pertanyaan - pertanyaan tersebut sering menjadi pertanyaan yang muncul 
+sebelum seseorang mempelajari Python. Berikut adalah beberapa di antara kelebihan Python:
+
+1.  Mudah dipelajari. Sintaksnya jelas dan mudah dibaca. Sangat cocok digunakan, terutama sebagai bahasa 
+    pemrograman pertama.
+2.  Simpel tapi powerful. Menulis kodingnya membutuhkan baris perintah yang lebih sedikit dibanding bahasa 
+    pemrograman lain.
+3.  Serbaguna, bisa untuk hampir apa saja kebutuhan atau ketertarikan Anda. Python bisa dipakai untuk pemrograman 
+    dekstop maupun mobile, CLI, GUI, web, otomatisasi, hacking, IoT, robotik, dan lain sebagainya.
+4.  Sangat populer. Rangking 3 di TIOBE index tahun 2020. No. 4 bahasa pemrograman paling banyak digunakan 
+    menurut Stackoverflow survey 2020. Merupakan no.1 bahasa pemrograman yang paling cepat perkembangannya 
+    dan paling diinginkan beberapa tahun belakangan menurut Stackoverflow. 
+5.  Siapa yang memakai python? Python banyak dipakai perusahaan-perusahaan besar dan top di dunia. 
+    Google menggunakannya di mesin pencarinya, di youtube dan lain-lain, microsoft, dropbox, instagram, 
+    pinterest, dan lain-lain.
+6.  Multi platform, python dapat dijalankan pada windows, linux, mac OS, Virtual Machine Java dan .NET.
+7.  Modul (library) python sangat banyak (berlimpah) sehingga dapat memudahkan kita untuk membuat program tanpa 
+    harus menulis kode dari dasar.
+8.  Python adalah bahasa pemrograman yang "kekinian" karena sedang naik daunnya bidang data science dan cabangnya 
+    seperti AI, machine learning, dan big data. Python paling populer dan banyak digunakan karena memiliki library 
+    yang lengkap untuk itu seperti sklearn, pytorch, tensorflow, dan lain sebagainya.
+9.  Open source. Python akan terus berkembang karena didukung oleh komunitas yang besar dan 
+    Lembaga Python Software Foundation (PSF) yang tiap tahunnya mengadakan konferensi internasional.
+10. Kode python bisa dijalankan secara interaktif (mode interactive) untuk testing, langsung nampak hasilnya.
+11. Multi paradigma, kita bisa menulis python dengan gaya pemrograman prosedural dan bisa juga dengan Object Oriented Programming. 
+    OOP-nya lebih mudah dan sederhana daripada bahasa seperti Java atau C++.
+12. Kode python bisa di-embed ke bahasa lain seperti C dan Java, atau sebaliknya, dari bahasa C atau Java ke Python.
+13. Python sangat cepat. Source code akan dikompile jadi bytecode, eksekusi file yang sama untuk kedua kalinya akan lebih cepat.
+14. Dokumentasi dan Tutorial mengenai bahasa pemrograman python tersedia melimpah, baik berbentuk buku cetak, e-book, artikel, 
+    video, dan forum tanya jawab. Semua permasalahan hampir dipastikan akan ada penyelesaiannya di internet.
+
+Tapi, itu hanya kelebihannya. Terus, apa kekurangannya? 
+
+Python adalah bahasa interpreter. Kekurangan python dibanding bahasa lain yang menggunakan kompiler 
+adalah "sedikit" lebih lambat pada saat dijalankan bila dibandingkan bahasa C maupun C++. Tapi hal 
+ini pun sangat bersifat relatif. Tergantung dari besar ukuran program yang dibuat.
+
+Untuk program besar yang membutuhkan kecepatan pemrosesan tinggi mungkin Python kalah cepat dari bahasa C, 
+tapi untuk hal selain itu Python lebih mudah dan lebih baik dari bahasa lain. Selain itu, kode sumber 
+sekarang sudah dioptimasi menggunakan bahasa C, sehingga kecepatannya juga sudah sangat mendekati kecepatan 
+bahasa C. Spesifikasi komputer juga sekarang ini sudah semakin tinggi sehingga bisa memproses program dengan 
+cepat, sehingga sering kali ini tidak menjadi hal penting dan bisa diabaikan.
 
 Sumber : https://www.pythonindo.com/
 '''
