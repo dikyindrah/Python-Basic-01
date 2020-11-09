7c485c4354160f84ba5485fdede31bb7e27e0114
dikyindrah <dikyindrah@gmail.com>
2020-11-09 21:20:28 +0700

    Menambahkan 02-Instalasi Python

---
+++ 02-Instalasi Python/Script.py
@@ -0,0 +1,11 @@
+'''
+PYTOHN 3.4
+----------
+https://www.petanikode.com/python-windows/
+https://www.pythonindo.com/cara-instalasi-python/
+
+
+PYTOHN 3.8 
+----------
+https://www.techcelup.com/cara-install-python-3-8-di-windows-10/
+'''
