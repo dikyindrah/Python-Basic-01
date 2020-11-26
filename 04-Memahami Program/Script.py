02386f8d63382555ed02152a4951e34e54f290a0
dikyindrah <dikyindrah@gmail.com>
2020-11-26 20:22:10 +0700

    Update 04-Memahami Program. Menambah file .txt dan .py

--- 04-Memahami Program/Script.py
+++ 04-Memahami Program/Script.py
@@ -3,11 +3,3 @@ print('Hello World')
 print('Hello World')
 print('Hello World')
 print('Hello World')
-
-# Python menggunakan teknik interpretasi untuk membaca dan mengeksekusi 
-# kode program baris perbaris.
-
-# Jika terjadi kesalahan kode program di suatu baris, maka
-# program langsung di hentikan pada baris yang mengalami kesalahan 
-# dan kode program pada baris selanjutnya tidak akan dibaca maupun 
-# dieksekusi.
