d55cfcb48e8e8979744939cbc098520af3063ee5
dikyindrah <dikyindrah@gmail.com>
2020-12-05 21:22:32 +0700

    Add 08-Komentar dan Docstring. Menambahkan file .txt dan .py

---
+++ 08-Komentar dan Docstring/Script.py
@@ -0,0 +1,26 @@
+print("Hello World")  # Menampilkan teks Hello World
+
+print("Hello World")
+# Menampilkan teks Hello World
+
+
+"""Menampilkan teks hello world"""
+print("Hello World")
+
+
+print("Hello World")
+"""Menampilkan teks hello world"""
+
+
+"""
+Menampilkan teks 
+hello world
+"""
+print("Hello World")
+
+
+print("Hello World")
+"""
+Menampilkan teks 
+hello world
+"""
