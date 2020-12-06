e773e7140dce5b1bd070761972458ed62174e9ce
dikyindrah <dikyindrah@gmail.com>
2020-12-06 20:58:15 +0700

    Update 09-Variabel dan Konstanta. Menambahkan file .txt dan .py

---
+++ 09-Variabel dan Konstanta/Script.py
@@ -0,0 +1,14 @@
+# Variable
+
+nama_depan = "Diky"
+nama_tengah = "Indra"
+nama_belakang = "Hermawanto"
+
+print(nama_depan, nama_tengah, nama_belakang)
+
+
+sisa_uang = 2000
+Sisa_Uang = 4000
+
+print(sisa_uang)
+print(Sisa_Uang)
