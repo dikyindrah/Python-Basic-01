49ad8fbbc20fd566ea6077b6820956f5c1b9a717
dikyindrah <dikyindrah@gmail.com>
2020-12-08 23:08:21 +0700

    Add 11-Tipe Data Text. Menambahkan file .txt dan .py

---
+++ 11-Tipe Data Text/Script.py
@@ -0,0 +1,48 @@
+# Tipe data teks
+# String
+
+username = 'diky'
+password = 'hello12345'
+
+print(username)
+print(password)
+
+username = "indrah"
+password = "world12345"
+
+print(username)
+print(password)
+
+
+nama = 'dikyindrah'
+print('\n' + nama)
+
+# Memeriksa panjang string
+print('Panjang string adalah :', len(nama), 'karakter')
+
+# Indeks
+# Dari depan
+print('\n' + nama[0])
+print(nama[1])
+print(nama[2])
+print(nama[3] + '\n')
+
+# Dari belakang
+print(nama[-1])
+print(nama[-2])
+print(nama[-3])
+print(nama[-4]+ '\n')
+
+# Substring
+# Dari depan
+print(nama[0:10])
+print(nama[3:7])
+print(nama[4:8])
+
+# Dari belakang
+print('\n' + nama[-4:-1])
+print(nama[-8:-3] + '\n')
+
+# Melewati beberapa karakter
+print(nama[0:10:3])
+print(nama[-10:-1:3])
