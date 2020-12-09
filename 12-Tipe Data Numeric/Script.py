1e470002e0a92dc7f37f3d3d9025d453f5a39197
dikyindrah <dikyindrah@gmail.com>
2020-12-09 21:05:58 +0700

    Update 12-Tipe Data Numeric. Memperbarui file .txt dan .py

---
+++ 12-Tipe Data Numeric/Script.py
@@ -0,0 +1,24 @@
+# Tipe data numeric
+
+# Integer
+nilai_ujian = 80
+print('Nilai ujian yang diperoleh adalah :', nilai_ujian)
+print('Data', nilai_ujian, 'memiliki tipe :', type(nilai_ujian))
+
+# Float
+berat_badan = 4.5
+print('\nBerat badan gintoki adalah', berat_badan, 'Kg')
+print('Data', berat_badan, 'memiliki tipe :', type(berat_badan))
+
+panjang_float = 2.123456789123456789123456789
+print('\nPanjang data', panjang_float, 'adalah', 
+        len(str(panjang_float)), 'karakter')
+
+hasil_hitung_nilai_float = 55.4104104148*78.8404174104
+print('\nPanjang data', hasil_hitung_nilai_float, 'adalah', 
+        len(str(hasil_hitung_nilai_float)), 'karakter')
+
+# Complex
+c = 1 * 5j
+print('c :', c)
+print('Data', c, 'memiliki tipe :', type(c))
