1ef00c0a1e53b3780e38a4c9fc0affa9eb033409
dikyindrah <dikyindrah@gmail.com>
2020-12-10 20:55:37 +0700

    Add 13-Tipe Data Boolean. Menambahkan file .txt dan .py

---
+++ 13-Tipe Data Boolean/Script.py
@@ -0,0 +1,21 @@
+# Tipe data boolean
+
+saya_lapar = True
+print('Apakah saya lapar :', saya_lapar)
+print('Data', saya_lapar, 'Memiliki tipe :', type(saya_lapar))
+
+saya_sedih = False
+print('\nApakah saya sedih :', saya_sedih)
+print('Data', saya_sedih  , 'Memiliki tipe :', type(saya_sedih  ))
+
+nilai_perbandingan = 12 > 10
+print('\nApakah 12 lebih besar dari 10 :', nilai_perbandingan)
+print('Data', nilai_perbandingan  , 'Memiliki tipe :', type(nilai_perbandingan  ))
+
+nilai_perbandingan = 12 < 10
+print('\nApakah 12 lebih kecil dari 10 :', nilai_perbandingan)
+print('Data', nilai_perbandingan  , 'Memiliki tipe :', type(nilai_perbandingan  ))
+
+nilai_perbandingan = 'A' == 'a'
+print('\nApakah A sama dengan a :', nilai_perbandingan)
+print('Data', nilai_perbandingan  , 'Memiliki tipe :', type(nilai_perbandingan  ))
