35da71a3091d2ee7e1376f61f684bc44c8dff639
dikyindrah <dikyindrah@gmail.com>
2020-12-19 21:57:53 +0700

    Update 15-Tipe Data Tidak Berurut. Memperbarui file .py

--- 15-Tipe Data Tidak Berurut/Script.py
+++ 15-Tipe Data Tidak Berurut/Script.py
@@ -3,6 +3,7 @@
 # Set
 print('\n==========Set==========\n')
 
+# Set dapat berisi item dengan berbagai macam tipe data
 # Set berisi integer
 set_integer = {23 ,45 ,12 ,88}
 
@@ -18,6 +19,7 @@ set_boolean = {True, False, True, False}
 # Set berisi tipe data campuran
 set_campuran = {23, 'HelloWorld!', True, 3.14}
 
+print('\nMenampilkan seluruh item pada set : \n')
 print('Set berisi integer  :', set_integer)
 print('Set berisi float    :', set_float)
 print('Set berisi string   :', set_string)
@@ -26,7 +28,7 @@ print('Set berisi campuran :', set_campuran)
 
 # Set berisi list
 set_list = set([1, 2, 3, 4, 5])
-print('\nSet berisi list  :', set_list)
+print('\nSet berisi list:', set_list)
 
 # list_integer = [10, 20, 30, 40, 50]
 # set_list = set(list_integer)
@@ -36,33 +38,50 @@ print('\nSet berisi list  :', set_list)
 set_kosong = set()
 
 # Set akan otomatis menhapus item yang sama
+print('\n\nSet akan otomatis menhapus item yang sama :')
+print('\nItem pada set        : 1, 2, 1, 3, 3, 4, 2, 5, 1, 3, 5, 6, 9, 8')
 set_integer = {1, 2, 1, 3, 3, 4, 2, 5, 1, 3, 5, 6, 9, 8}
-print('\n', set_integer)
+print('Saat item di tampilkan :', set_integer)
 
 # Menambahakan satu item pada set
+print('\n\nMenambahakan satu item pada set :')
 set_nama = {'diky', 'indra'}
+print('\nSebelum item ditambah :',set_nama)
+
 set_nama.add('hermawanto')
-print('\n', set_nama)
+print('\nSetelah item ditambah :', set_nama)
 
 # Menambahkan beberapa item sekaligus
+print('\n\nMenambahkan beberapa item sekaligus :')
 set_angka_ganjil = {3, 5, 7}
+print('\nSebelum item ditambah :',set_angka_ganjil)
 set_angka_ganjil.update({3, 5, 7, 13, 15, 17})
-print('\n', set_angka_ganjil)
+print('\nSetelah item ditambah :', set_angka_ganjil)
 
 # Menghapus item pada set
+print('\n\nMenghapus item pada set :')
 # Discard
+print('\nMenghapus item menggunakan discard :')
 set_makanan = {'Ayam Goreng', 'Rendang', 'Opor'}
+print('\nItem sebelum di hapus :', set_makanan)
+# Item yang ingin di hapus ada didalam set
 set_makanan.discard('Ayam Goreng')
-print('\n', set_makanan)
+print('\nItem setelah di hapus :', set_makanan)
 
+# Item yang ingin di hapus tidak ada didalam set
+print('\nMenghapus item yang tidak ada didalam set :')
 set_makanan.discard('Pecel Lele')
 print('\n', set_makanan)
 
-
 # Remove
+print('\n\nMenghapus item menggunakan remove :')
 set_minuman = {'Es Teh', 'Kopi', 'Teh Hangat'}
+print('\nItem sebelum di hapus :', set_minuman)
+# Item yang ingin di hapus ada didalam set
 set_minuman.remove('Kopi')
-print('\n', set_minuman)
+print('\nItem setelah di hapus :', set_minuman)
 
+# Item yang ingin di hapus tidak ada didalam set
+print('\nMenghapus item yang tidak ada didalam set :\n')
 set_minuman.remove('Air Putih')
 print('\n', set_minuman)
