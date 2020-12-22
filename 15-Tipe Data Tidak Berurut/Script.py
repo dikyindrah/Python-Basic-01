d78609a4c8d2b2d18ad6822007b64182e1258289
dikyindrah <dikyindrah@gmail.com>
2020-12-22 23:52:04 +0700

    Update 15-Tipe Data Tidak Berurut. Memperbarui file .txt dan .py

--- 15-Tipe Data Tidak Berurut/Script.py
+++ 15-Tipe Data Tidak Berurut/Script.py
@@ -19,6 +19,9 @@ set_boolean = {True, False, True, False}
 # Set berisi tipe data campuran
 set_campuran = {23, 'HelloWorld!', True, 3.14}
 
+# Memeriksa apakah suatu item ada pada set
+print(23 in set_integer)
+
 print('\nMenampilkan seluruh item pada set : \n')
 print('Set berisi integer  :', set_integer)
 print('Set berisi float    :', set_float)
@@ -36,6 +39,9 @@ print('\nSet berisi list:', set_list)
 
 # Membuat set kosong
 set_kosong = set()
+
+# Memeriksa apakah suatu item ada pada set
+print(23 in set_integer)
 
 # Set akan otomatis menhapus item yang sama
 print('\n\nSet akan otomatis menhapus item yang sama :')
@@ -89,3 +95,155 @@ print('\nItem setelah di hapus :', set_minuman)
 
 # Dictionary
 print('\n\n==========Dictionary==========\n')
+
+# Dictionary dapat berisi item dengan berbagai macam tipe data
+
+# Key pada dictionary bisa dibuat dangan berbagai tipe data kecuali 
+# list, set, dan dictionary.
+
+# Key dibuat dengan integer
+dictionary_key_integer = {
+    1:'processor', 
+    2:'motherboard', 
+    3:'harddisk', 
+    4:'ram', 
+    5:'power suply', 
+    6:'fan'
+}
+
+# Key dibuat dengan float
+dictionary_key_float = {21.5:'35 cm', 23.5:'38 cm', 26.5:'42 cm'}
+
+# Key dibuat dengan string
+dictionary_key_string = {'deni':19, 'diky':21, 'rudi':25}
+
+# Key dibuat dengan boolean
+dictionary_key_boolean = {True:'Yes', False:'No'}
+
+# Key dibuat dengan tuple
+dictionary_key_tuple = {(1,):("A"), (2,):("B"), (3):("C")} 
+
+# Key dibuat dengan campuran
+dictionary_key_campuran = {1:'HelloWorld', 'warna':'merah', True:1, 85.5:'Baik', (1,):("A")}
+
+
+# Value pada dictionary bisa dibuat dangan berbagai tipe data
+# termasuk list, set, dan dictionary.
+
+# Dictionary dengan value integer
+dictionary_value_integer = {'Ayah':45, 'Ibu':40, 'Kakak':18, 'Adik':14}
+
+# Dictionary dengan value float
+dictionary_value_float = {'diky':145.5, 'bagus':165.5}
+
+# Dictionary dengan value string
+dictionary_value_string = {1:'Diky', 2:'Indra', 3:'Hermawanto'} 
+
+# Dictionary dengan value boolean
+dictionary_value_boolean = {0:False, 1:True}
+
+# Dictionary dengan value list
+dictionary_value_list = {
+    1:['gintoki', 'kagura', 'shinpachi'], 
+    2:['hijikata', 'yamazaki', 'okita']
+}
+
+# Dictionary dengan value tuple
+dictionary_value_tuple = {
+    1:('gintoki', 'kagura', 'shinpachi'), 
+    2:('hijikata', 'yamazaki', 'okita')
+}
+
+# Dictionary dengan value set
+dictionary_value_set = {
+    1:{'gintoki', 'kagura', 'shinpachi'}, 
+    2:{'hijikata', 'yamazaki', 'okita'}
+}
+
+# Dictionary dengan value dictionary
+dictionary_value_dictionary = {
+    1:{1:'gintoki', 2:'kagura', 3:'shinpachi'}, 
+    2:{1:'hijikata', 2:'yamazaki', 3:'okita'}
+}
+
+# Dictionary dengan value campuran
+dictionary_value_campuran = {
+    1:21,
+    2:175.5,
+    3:'dikyindrah',
+    4:True,
+    5:['Bandar Lampung'],
+    6:('Web Developer'),
+    7:{'Success'},
+    8:{1:'Happy'}
+}
+
+print('\nMenampilkan seluruh item pada dictionary : \n')
+print('Key pada dictionary bisa dibuat dangan berbagai tipe data kecuali\nlist, set, dan dictionary.\n')
+print('Key dibuat dengan integer  :', dictionary_key_integer)
+print('Key dibuat dengan float    :', dictionary_key_float)
+print('Key dibuat dengan string   :', dictionary_key_string)
+print('Key dibuat dengan boolean  :', dictionary_key_boolean)
+print('Key dibuat dengan campuran :', dictionary_key_campuran)
+
+print('\nValue pada dictionary bisa dibuat dangan berbagai tipe data termasuk\nlist, set, dan dictionary.\n')
+print('Value dibuat dengan integer  :', dictionary_value_integer)
+print('Value dibuat dengan float    :', dictionary_value_float)
+print('Value dibuat dengan string   :', dictionary_value_string)
+print('Value dibuat dengan boolean  :', dictionary_value_boolean)
+print('Value dibuat dengan list     :', dictionary_value_list)
+print('Value dibuat dengan tuple    :', dictionary_value_tuple)
+print('Value dibuat dengan set      :', dictionary_value_set)
+print('Value dibuat dengan dictionary  :', dictionary_value_dictionary)
+print('Value dibuat dengan campuran :', dictionary_value_campuran)
+
+# Mengakses item pada dictionary
+print('\n\nMengakses item pada dictionary :\n')
+
+usia_anggota_keluarga = {'Ayah':45, 'Ibu':40, 'Kakak':18, 'Adik':14}
+
+# Memeriksa apakah suatu item ada pada dictionary
+print('Ayah' in usia_anggota_keluarga, '\n')
+
+# Mengakses item pada dictionary menggunakan cara manual
+print('Mengakses item pada dictionary menggunakan cara manual :\n')
+
+# Akses item pada dictionary ketika key ada dalam dictionary
+print('Usia ibu saat ini adalah', usia_anggota_keluarga['Ibu'], 'tahun')
+
+# # Akses item pada dictionary ketika key tidak ada dalam dictionary
+# print('Usia nenek saat ini adalah', usia_anggota_keluarga['Nenek'], 'tahun')
+
+# Mengakses item pada dictionary menggunakan fungsi get()
+print('\nMengakses item pada dictionary menggunakan fungsi get() :\n')
+
+# Akses item pada dictionary ketika key ada dalam dictionary
+print('Usia ayah saat ini adalah', usia_anggota_keluarga.get('Ayah'), 'tahun')
+
+# Akses item pada dictionary ketika key tidak ada dalam dictionary
+print('Usia nenek saat ini adalah', usia_anggota_keluarga.get('Nenek'), 'tahun')
+
+# Mengubah item pada dictionary
+print('\n\nMengubah item pada dictionary :\n')
+
+data_diri = {'Nama':'Diky Indra H', 'Umur':21}
+print('Sebelum diubah :', data_diri)
+
+# Mengubah item pada dictionary
+data_diri['Umur'] = 22
+
+# Menghapus item pada dictionary
+print('\n\nMenghapus item pada dictionary :\n')
+
+data_diri = {'Nama':'Diky Indra H', 'Umur':21}
+print('Sebelum dihapus :', data_diri, '\n')
+
+# Menghapus item tertentu pada dictionary
+data_diri.pop('Umur')
+print('Menghapus salah satu item :', data_diri)
+
+# Menghapus seluruh item pada dictionary
+data_diri.clear()
+print('Menghapus seluruh item :', data_diri, '\n')
+
+
