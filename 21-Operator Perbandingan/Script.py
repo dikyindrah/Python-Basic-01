24ed7ba287d3e3a7d0331b7bed2f85196fbf223c
dikyindrah <dikyindrah@gmail.com>
2020-12-31 21:07:27 +0700

    Add 21-Operator Perbandingan. Menambahkan file .txt dan .py

---
+++ 21-Operator Perbandingan/Script.py
@@ -0,0 +1,15 @@
+# Operator perbandingan
+
+print('\n==========Operator Perbandingan==========\n')
+
+x = int(input('Masukan nilai x: '))
+y = int(input('Masukan nilai y: '))
+
+print('')
+print(x, '==', y, ' =', (x==y))
+print(x, '!=', y, ' =', (x!=y))
+print(x, '<', y, '  =', (x<y))
+print(x, '>', y, '  =', (x>y))
+print(x, '<=', y, ' =', (x<=y))
+print(x, '>=', y, ' =', (x>=y))
+print('')
