d05648d05d5b4befbdf2d2eb1c3c3afeaff01173
dikyindrah <dikyindrah@gmail.com>
2020-12-31 21:06:05 +0700

    Add 20-Operator Aritmatika. Menambahkan file .txt dan .py

---
+++ 20-Operator Aritmatika/Script.py
@@ -0,0 +1,16 @@
+# Operator aritmatika
+
+print('\n==========Operator Aritmatika==========\n')
+
+x = int(input('Masukan nilai x: '))
+y = int(input('Masukan nilai y: '))
+
+print('')
+print(x, '+', y, '  =', (x+y))
+print(x, '-', y, '  =', (x-y))
+print(x, '*', y, '  =', (x*y))
+print(x, '/', y, '  =', (x/y))
+print(x, '//', y, ' =', (x//y))
+print(x, '%', y, '  =', (x%y))
+print(x, '**', y, ' =', (x**y))
+print('')
