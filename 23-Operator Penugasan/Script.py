509b14a8077689ceee89f6c9735901db8514c933
dikyindrah <dikyindrah@gmail.com>
2021-01-01 17:09:28 +0700

    Add 23-Operator Penugasan. Menambahkan file .txt dan .py

---
+++ 23-Operator Penugasan/Script.py
@@ -0,0 +1,51 @@
+# Operator penugasan
+
+print('\n==========Operator Penugasan==========\n')
+
+print('|  =  |')
+x = int(input('Masukan nilai x: '))
+y = int(input('Masukan nilai y: '))
+x=y
+print('x=y  =', x)
+
+print('\n| +=  |')
+x = int(input('Masukan nilai x: '))
+y = int(input('Masukan nilai y: '))
+x+=y
+print('x+=y =', x)
+
+print('\n| -=  |')
+x = int(input('Masukan nilai x: '))
+y = int(input('Masukan nilai y: '))
+x-=y
+print('x-=y =', x)
+
+print('\n| *=  |')
+x = int(input('Masukan nilai x: '))
+y = int(input('Masukan nilai y: '))
+x*=y
+print('x*=y =', x)
+
+print('\n| /=  |')
+x = int(input('Masukan nilai x: '))
+y = int(input('Masukan nilai y: '))
+x/=y
+print('x/=y =', x)
+
+print('\n| //= |')
+x = int(input('Masukan nilai x: '))
+y = int(input('Masukan nilai y: '))
+x//=y
+print('x//=y =', x)
+
+print('\n| %=  |')
+x = int(input('Masukan nilai x: '))
+y = int(input('Masukan nilai y: '))
+x%=y
+print('x%=y =', x)
+
+print('\n| **= |')
+x = int(input('Masukan nilai x: '))
+y = int(input('Masukan nilai y: '))
+x**=y
+print('x**=y =', x)
