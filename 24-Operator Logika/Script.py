d96ac77738295027eeac02da1fccab4d85e17cb7
dikyindrah <dikyindrah@gmail.com>
2021-01-01 17:10:11 +0700

    Add 24-Operator Logika. Menambahkan file .txt dan .py

---
+++ 24-Operator Logika/Script.py
@@ -0,0 +1,29 @@
+# Operator logika
+
+print('\n==========Operator Logika=========\n')
+
+print('AND\n')
+print('True and True   =', (True and True))
+print('True and False  =', (True and False))
+print('False and False =', (False and False))
+print('False and True  =', (False and True))
+
+print('\n\nOR\n')
+print('True or True   =', (True or True))
+print('True or False  =', (True or False))
+print('False or False =', (False or False))
+print('False or True  =', (False or True))
+
+print('\n\nNOT\n')
+print('not True   =', (not True))
+print('not False  =', (not False))
+
+print('\nnot (True and True)   =', (not(True and True)))
+print('not (True and False)  =', (not(True and False)))
+print('not (False and False) =', (not(False and False)))
+print('not (False and True)  =', (not(False and True)))
+
+print('\nnot (True or True)   =', (not(True or True)))
+print('not (True or False)  =', (not(True or False)))
+print('not (False or False) =', (not(False or False)))
+print('not (False or True)  =', (not(False or True)))
