3c8d75c20aa66db237cc86b22281eb2e259510a5
dikyindrah <dikyindrah@gmail.com>
2021-01-02 19:20:50 +0700

    Add 28-Latihan Membuat Program Untuk Menghitung Luas dan Keliling Persegi Panjang. Menambahkan file .py

---
+++ 28-Latihan Membuat Program Untuk Menghitung Luas dan Keliling Persegi Panjang/Script.py
@@ -0,0 +1,12 @@
+# Program untuk menghitung luas dan keliling persegi panjang
+
+print('\n==========Program Hitung Luas dan Keliling Persegi Panjang==========\n')
+
+panjang = float(input('Masukan panjang : '))
+lebar = float(input('Masukan lebar   : '))
+
+luas = panjang * lebar
+keliling = (2 * panjang) + (2 * lebar)
+
+print('\nLuas persegi panjang adalah', luas)
+print('Keliling persegi panjang adalah', keliling, '\n')
