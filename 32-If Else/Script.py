1a1359295a6518cd567aae29c8fa009a50089c12
dikyindrah <dikyindrah@gmail.com>
2021-01-03 21:08:29 +0700

    Add 32-If Else. Menambahkan file .txt dan .py

---
+++ 32-If Else/Script.py
@@ -0,0 +1,10 @@
+# If Else
+
+print('\n==========If Else==========\n')
+
+angka = int(input('Masukan angka: '))
+
+if angka % 2 == 1:
+    print(angka, 'adalah bilangan ganjil!')
+else:
+    print(angka, 'adalah bilangan genap!')
