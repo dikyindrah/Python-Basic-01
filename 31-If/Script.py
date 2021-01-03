d0a73f9546b06f8d869c6ff76eb50b89fb3110ae
dikyindrah <dikyindrah@gmail.com>
2021-01-03 21:07:49 +0700

    Add 31-If. Menambahkan file .txt dan .py

---
+++ 31-If/Script.py
@@ -0,0 +1,15 @@
+# If
+
+print('\n==========If==========\n')
+
+saya = 'lapar'
+
+if saya == 'lapar':
+    print('Saya ingin makan!\n')
+
+angka = int(input('Masukan angka: '))
+
+if angka > 0:
+    print(angka, 'adalah bilangan positif')
+
+
