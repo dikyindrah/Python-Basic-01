8c5fe01aa31e369483b97ceacaaebe038c3b4738
dikyindrah <dikyindrah@gmail.com>
2021-01-06 22:57:39 +0700

    Add 36-For. Menambahkan file .txt dan .py

---
+++ 36-For/Script.py
@@ -0,0 +1,19 @@
+# For
+
+print('\n==========For==========\n')
+
+print('\n| Tampilkan 5 angka |\n')
+for i in range(5):
+    print(i)
+
+print('\n| Tampilkan angka 1 sampai 5 |\n')
+for i in range(1, 5+1):
+    print(i)
+
+print('\n| Tampilkan 5 angka secara ascending |\n')
+for i in range(0, 5, 1):
+    print(i)
+
+print('\n| Tampilkan 5 angka secara descending |\n')
+for i in range(5, 0, -1):
+    print(i)
