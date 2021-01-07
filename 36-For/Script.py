6f3d029d98500aa750cd5d2289504d34494114d2
dikyindrah <dikyindrah@gmail.com>
2021-01-07 16:48:34 +0700

    Update 36-For. Memperbarui file .txt dan .py

--- 36-For/Script.py
+++ 36-For/Script.py
@@ -17,3 +17,21 @@ for i in range(0, 5, 1):
 print('\n| Tampilkan 5 angka secara descending |\n')
 for i in range(5, 0, -1):
     print(i)
+
+list_hari = [
+    'Senin', 
+    'Selasa', 
+    'Rabu', 
+    'Kamis', 
+    'Jumat', 
+    'Sabtu', 
+    'Minggu'
+]
+
+print('\n| Tampilkan seluruh item pada list |\n')
+for hari in list_hari:
+    print(hari)
+
+print('\n| Tampilkan seluruh item pada list berdasarkan indeks |\n')
+for hari in range(len(list_hari)):
+    print('Indeks ke', hari, 'adalah hari', list_hari[hari])
