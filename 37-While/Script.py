96e344dbeea3d291e61f509adc9550413bc751ff
dikyindrah <dikyindrah@gmail.com>
2021-01-07 22:02:17 +0700

    Add 37-While. Menambahkan file .txt dan .py

---
+++ 37-While/Script.py
@@ -0,0 +1,15 @@
+# While
+
+print('\n==========While==========\n')
+
+# x = 0
+# while (x < 5):
+#     print(x)
+#     x = x + 1
+
+x = 6
+while (x < 5):
+    print(x)
+    x = x + 1
+else:
+    print('\nNilai x harus kurang dari 5\n')
