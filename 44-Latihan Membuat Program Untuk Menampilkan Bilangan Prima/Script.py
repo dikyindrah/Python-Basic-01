96f1bbe531cbd81a5a9e20a0321557e08a679412
dikyindrah <dikyindrah@gmail.com>
2021-01-25 17:26:08 +0700

    Add 44-Latihan Membuat Program Untuk Menampilkan Bilangan Prima. Menambahkan file .py

---
+++ 44-Latihan Membuat Program Untuk Menampilkan Bilangan Prima/Script.py
@@ -0,0 +1,23 @@
+# Program untuk menampilkan deret bilangan prima
+
+print('\n==========Deret Blangan Prima==========\n')
+
+# Cara pertama
+for i in range(2, 10):
+    for j in range(2, i):
+        if i % j == 0:
+            break
+    else:
+        print(i, end=' ')
+
+print('\n')
+
+# Cara kedua
+for i in range(2, 10):
+    prima = True
+    for j in range(2, i):
+        if i % j == 0:
+            prima = False   
+
+    if prima == True:
+        print(i, end=' ')
