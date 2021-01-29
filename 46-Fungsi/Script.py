25e3a6f9d34692330ddce195a71bc2a4098abca1
dikyindrah <dikyindrah@gmail.com>
2021-01-29 22:31:26 +0700

    Add 46-Fungsi. Menambahkan file .txt dan .py

---
+++ 46-Fungsi/Script.py
@@ -0,0 +1,12 @@
+# Fungsi
+
+# Fungsi katakan_halo()
+def katakan_halo():
+    '''Fungsi ini dubuat untuk menampilkan teks halo!'''
+    print('halo!')
+
+# Memanggil fungsi katakan_halo()
+katakan_halo()
+
+# Mengakses docstring pada fungsi katakan_halo()
+print(katakan_halo.__doc__)
