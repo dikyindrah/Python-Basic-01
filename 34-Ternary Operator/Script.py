0ec3ad49272e00c4fe27610f7a2e5bfacb6aad44
dikyindrah <dikyindrah@gmail.com>
2021-02-02 18:02:44 +0700

    Add 34-Ternary Operator. Menambahkan file .txt dan .py

---
+++ 34-Ternary Operator/Script.py
@@ -0,0 +1,30 @@
+# Ternary operator
+a = 10
+b = 20
+
+# Ternary operator if
+hasil = 'a lebih kecil dari b' if a < b else 'a lebih besar dari b'
+print(hasil)
+
+'''
+# Nested if
+if a == b:
+    print('a sama dengan b')
+else:
+    if a < b:
+        print('a lebih kecil dari b')
+    else:
+        print('a lebih besar dari b')
+'''
+
+# Ternary operator nested if
+hasil = 'a sama dengan b' if a == b else 'a lebih kecil dari b' if a < b else 'a lebih besar dari b'
+print(hasil)
+
+# Seleksi item pada tuple menggunakan ternary operator
+hasil = (b, a) [a < b]
+print(hasil)
+
+# Seleksi item pada dictonary menggunakan ternary operator
+hasil = {True:a, False:b} [a < b]
+print(hasil)
