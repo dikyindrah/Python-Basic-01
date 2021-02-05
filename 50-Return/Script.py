afbcef4918b10edf271f3e58b9273bb63eb0ce47
dikyindrah <dikyindrah@gmail.com>
2021-02-05 21:58:49 +0700

    Rename 49-Return and Update. Mengubah nama folder menjadi 50-Return serta memperbarui file .txt dan .py

---
+++ 50-Return/Script.py
@@ -0,0 +1,24 @@
+# Return
+
+# Return dengan ekspresi
+def penjumlahan(x, y):
+    return x + y
+
+# Return tidak dengan ekspresi
+def perkalian(x, y):
+    return
+
+# Memanggil fungsi yang memiliki pernyataan return dengan ekspresi
+hasil_penjumlahan = penjumlahan(5, 5)
+
+# Menampilkan hasil eksekusi fungsi
+print(hasil_penjumlahan)
+
+# Mengolah dan menampilkan hasil eksekusi fungsi
+print(hasil_penjumlahan+10)
+
+# Memanggil fungsi yang memiliki pernyataan return tanpa ekspresi
+hasil_perkalian = perkalian(2, 5)
+
+# Menampilkan hasil eksekusi fungsi
+print(hasil_perkalian)
