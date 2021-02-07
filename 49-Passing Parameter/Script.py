a0893cf9f4d0371f5f3dfc8ab4c45fd199ea0a03
dikyindrah <dikyindrah@gmail.com>
2021-02-07 12:48:49 +0700

    Add 49-Passing Parameter. Menambahakan file .txt dan .py

---
+++ 49-Passing Parameter/Script.py
@@ -0,0 +1,44 @@
+# Passing paramenter
+
+print('==========Passing Parameter==========\n')
+
+print('Pass by Value\n')
+nilai = 10
+
+def penjumlahan(x):
+    print(x + 10)
+
+# Sebelum memanggil fungsi penjumalan()
+print(nilai)
+
+# Saat memanggil fungsi penjumalan()
+penjumlahan(nilai)
+
+# Setelah memanggil fungsi penjumalan()
+print(nilai)
+
+# Mengubah nilai pada variabel asli
+nilai = 50
+print(nilai)
+
+print('\nPass by Reference\n')
+
+list_angka = [1, 2, 3, 4, 5]
+
+def ubah_angka(angka):
+    angka[4] = 71
+    return angka
+
+# Sebelum memanggil fungsi ubah_angka()
+print(list_angka)
+
+# Saat memanggil fungsi ubah_angka()
+print(ubah_angka(list_angka))
+
+# SEtelah memanggil fungsi ubah_angka()
+print(list_angka)
+
+# Mengubah angka pada variable asli
+list_angka[1] = 155
+print(list_angka)
+print(ubah_angka(list_angka))
