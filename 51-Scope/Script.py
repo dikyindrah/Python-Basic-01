5f770869645867dbeed9035b91092f1f4e4c9f8b
dikyindrah <dikyindrah@gmail.com>
2021-02-08 20:48:48 +0700

    Update 51-Scope. Memperbarui file .txt dan .py

--- 51-Scope/Script.py
+++ 51-Scope/Script.py
@@ -45,3 +45,36 @@ print(min(list_angka))
 print(max(list_angka))
 print(sum(list_angka))
 print(sorted(list_angka))
+
+# Nonlokal
+
+def f1():
+    teks = 'Hello World'
+
+    def f2():
+        nonlocal teks 
+        teks = 'Hello Pyhton'
+        print(teks)
+
+    # Sebelum memanggil fungsi f2()
+    print(teks)
+
+    # Saat memanggil fungsi f2()
+    f2()
+    
+    # Stelah memanggil fungsi f2()
+    print(teks)
+
+f1()
+
+def f1():
+    num = 1
+
+    def f2():
+        num = num + 2
+        print(num)
+    
+    f2()
+    print(num)
+
+f1()
