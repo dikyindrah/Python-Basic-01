1ad2089f77ec0a00b586751271aa2415efc779a1
dikyindrah <dikyindrah@gmail.com>
2021-02-21 20:59:08 +0700

    Update 53-Memanggil Fungsi Di Dalam Fungsi

--- 53-Memangil Fungsi Di Dalam Fungsi/Script.py
+++ 53-Memangil Fungsi Di Dalam Fungsi/Script.py
@@ -26,3 +26,26 @@ def tampilkan_isi_list():
 
 # Memanggil fungsi tampilkan_isi_list()
 tampilkan_isi_list()
+
+print('\n')
+
+def pesan_minuman(nomor_minuman):
+    def es_teh():
+        print('Anda memesan es teh')
+
+    def jus_jeruk():
+        print('Anda memesan jus jeruk')
+
+    if nomor_minuman == 1:
+        # Memanggil fungsi es_teh() dan menggunakannya sebagai 
+        # nilai kembali
+        return es_teh()
+    elif nomor_minuman == 2:
+        # Memanggil fungsi jus_jeruk() dan menggunakannya 
+        # sebagai nilai kembali
+        return jus_jeruk()
+    else:
+        print('Maaf, minuman yang anda pesan tidak tersedia!')
+
+# Memanggil fungsi pesan_minuman()
+pesan_minuman(1)
