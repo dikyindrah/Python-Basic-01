b8663f8f54766b65c7e2f98484af60911f36920c
dikyindrah <dikyindrah@gmail.com>
2021-02-27 00:54:31 +0700

    Update 56-Decorator

--- 56-Decorator/Script.py
+++ 56-Decorator/Script.py
@@ -19,12 +19,13 @@ print(hasil())
 
 print('')
 
-# def ucap_selamat_malam(fungsi):
+# # Cara kerja decorator
+# def ucap_selamat_malam(fungsi): # <- Mengambil fungsi
 #     def ucap():
 #         print('Selamat Malam!')
-#         fungsi()
+#         fungsi() # <- Menambahakan fungsi
     
-#     return ucap
+#     return ucap # <- Mengembalikan fungsi
 
 # def ucap_selamat_beristirahat():
 #     print('Selamat Beristirahat!')
@@ -36,6 +37,7 @@ print('')
 # ucapan = ucap_selamat_malam(ucap_selamat_beristirahat)
 # ucapan()
 
+# Cara memanggil decorator
 def ucap_selamat_malam(fungsi):
     def ucap():
         print('Selamat Malam!')
@@ -55,6 +57,7 @@ ucap_selamat_beristirahat()
 
 print('')
 
+# Menggunakan parameter untuk mengambil parameter fungsi
 def hitung_pembagian(fungsi):
     def hitung(x, y):
         if x == 0 or y == 0:
@@ -70,3 +73,71 @@ def input_nilai(x, y):
 
 hasil_pembagian = input_nilai
 print(hasil_pembagian(5, 5))
+
+print('')
+
+# Mengambil sejumlah argumen menggunakan *args
+def hitung_total_pendapatan_bersih(fungsi):
+    def hitung(*args):
+        total_pendapatan = fungsi(*args)
+        pendapatan_bersih = total_pendapatan - 150000
+        return pendapatan_bersih
+    
+    return hitung
+
+@hitung_total_pendapatan_bersih
+def hitung_total_pendapatan(*args):
+    total = 0 
+    for item in args:
+        total = total + item
+    
+    return total
+
+print(hitung_total_pendapatan(100000,100000,100000,100000,100000))
+
+print('')
+
+# Mengambil sejumlah argumen menggunakan *kwargs
+def tambah_keterangan_informasi(fungsi):
+    def ubah(**kwargs):
+        print('=' * 10, 'Data Diri', '=' * 10 )
+        fungsi(**kwargs)
+    
+    return ubah
+
+@tambah_keterangan_informasi
+def tampilkan_informasi(**kwargs):
+    for key, value in kwargs.items():
+        print(key, '=', value) 
+    
+tampilkan_informasi(Nama='dikyindrah', Umur=21, Alamat='Bandar Lampung')
+
+print('')
+
+# fungsi dapat didekorasi beberapa kali dengan dekorator 
+# yang berbeda atau sama
+def star(func):
+    def inner(*args, **kwargs):
+        print("*" * 30)
+        func(*args, **kwargs)
+        print("*" * 30)
+    return inner
+
+def percent(func):
+    def inner(*args, **kwargs):
+        print("%" * 30)
+        func(*args, **kwargs)
+        print("%" * 30)
+    return inner
+
+
+@star
+@percent
+def printer(msg):
+    print(msg)
+
+printer("Hello")
+
+# Cara di atas sama dengan cara di baswah ini
+# p = star(percent(printer))
+# p('Hello')
