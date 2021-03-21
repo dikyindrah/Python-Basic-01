48638a018b43e732744cecdc92343f389297c66b
dikyindrah <dikyindrah@gmail.com>
2021-03-21 22:10:59 +0700

    66-Menambah, Mengubah, dan Menghapus Item Pada List dan Tuple

--- 66-Menambah, Mengubah, dan Menghapus Item Pada List dan Tuple/Script.py
+++ 66-Menambah, Mengubah, dan Menghapus Item Pada List dan Tuple/Script.py
@@ -1,6 +1,8 @@
-# Menambah, mengubah, dan menghapus item pada list
+# Menambah, mengubah, dan menghapus item pada list dan tuple
 
-print('\n==========Menambah, Mengubah, dan Menghapus Item Pada List==========\n')
+print('\n==========Menambah, Mengubah, dan Menghapus Item Pada List dan Tuple==========\n')
+
+print('\n==Menambah, Mengubah, dan Menghapus Item Pada List=====\n')
 
 list_integer = [10, 20, 30, 40, 50]
 print('list_integer = ', list_integer)
@@ -25,4 +27,19 @@ print('list_integer = ', list_integer)
 print('\nMenghapus Seluruh Item:')
 list_integer.clear()
 print('list_integer = ', list_integer)
+
+print('\n==Menambah, Mengubah, dan Menghapus Item Pada Tuple=====\n')
+
+tuple_string = ('aku', 'sedang')
+print(tuple_string)
+
+list_string = list(tuple_string)
+
+list_string.append('belajar')
+list_string.append('python')
+print(list_string)
+
+tuple_string = tuple(list_string)
+print(tuple_string)
+
 
