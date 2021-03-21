069466d6a133d0105bbf5ca361e48046ab7d974d
dikyindrah <dikyindrah@gmail.com>
2021-03-21 22:08:26 +0700

    Update 64-List dan Tuple

---
+++ 64-List dan Tuple/Script.py
@@ -0,0 +1,33 @@
+# List dan tuple
+
+print('\n==========List==========\n')
+
+list_kosong = []
+list_integer = [1, 2, 3, 4, 5]
+list_float = [2.5, 1.5, 10.10, 7.8]
+list_boolean = [True, True, False, False]
+list_string = ['aku', 'sedang', 'belajar', 'python']
+list_campuran = [56, 'python', 5.5, True]
+
+print(list_kosong)
+print(list_integer)
+print(list_float)
+print(list_boolean)
+print(list_string)
+print(list_campuran)
+
+print('\n==========Tuple==========\n')
+
+tuple_kosong = ()
+tuple_integer = (1, 2, 3, 4, 5)
+tuple_folat = (5.5, 6.0, 21.5, 33.4, 10.8)
+tuple_boolean = (True, False, True, False)
+tuple_string = ('aku', 'sedang', 'belajar', 'python')
+tuple_campuran = (10, 'python', 5.5, False)
+
+print(tuple_kosong)
+print(tuple_integer)
+print(tuple_folat)
+print(tuple_boolean)
+print(tuple_string)
+print(tuple_campuran)
