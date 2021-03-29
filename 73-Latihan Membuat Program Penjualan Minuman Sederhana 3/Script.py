d26743957baf87958aecea7fef608fd4a4823f75
dikyindrah <dikyindrah@gmail.com>
2021-03-29 23:36:56 +0700

    Update 73-Latihan Membuat Program Penjualan Minuman Sederhana 3

--- 73-Latihan Membuat Program Penjualan Minuman Sederhana 3/Script.py
+++ 73-Latihan Membuat Program Penjualan Minuman Sederhana 3/Script.py
@@ -1,79 +1,91 @@
-# Latihan membuat program penjualan minuman sederhana 3
 from prettytable import PrettyTable
 
-def hitung_total_pembelian(minuman_user, harga_minuman):
-        total_harga = 0
-        for i in range(len(minuman_user)):
-                nomor = minuman_user[i]
-                total_harga = total_harga + harga_minuman[nomor]
-        return total_harga
-
-def hitung_uang_kembalian(jumlah_uang):
-        total_harga_pembelian = hitung_total_pembelian(minuman_user, harga_minuman)
-        if jumlah_uang < total_harga_pembelian:
-                return 'Maaf, Uang anda tidak cukup.'
-        else:
-                return jumlah_uang - total_harga_pembelian
-
-def keterangan_pembelian(minuman_user, harga_minuman, total_uang):
-        total_pembelian = hitung_total_pembelian(minuman_user, harga_minuman)
-        uang_kembalian = hitung_uang_kembalian(total_uang)
-        tabel_keterangan_pembelian = PrettyTable()
-        tabel_keterangan_pembelian.field_names = ['Nama Minuman', 'Harga']
-        tabel_keterangan_pembelian.align['Nama Minuman'] = 'l'
-        tabel_keterangan_pembelian.align['Harga'] = 'r'
-
-        for i in range(len(minuman_user)):
-                nomor = minuman_user[i]
-                tabel_keterangan_pembelian.add_row(nama_minuman[nomor], harga_minuman[i])
-        
-        print(tabel_keterangan_pembelian)
-
-
 def main():
-        tabel_minuman = PrettyTable()
-        tabel_minuman.field_names = ['No','Nama Minuman', 'Harga']
-        tabel_minuman.align['No'] = 'c'
-        tabel_minuman.align['Nama Minuman'] = 'l'
-        tabel_minuman.align['Harga'] = 'r'
+    nama_minuman = ['Sprite', 
+                    'Coca-cola', 
+                    'Mizone', 
+                    'Pocari Sweat', 
+                    'Teh Botol Sosro']
 
-        jumlah_item = int(((len(nama_minuman) + len(harga_minuman))/2))
-        i = 0
-        while i < jumlah_item:
-                tabel_minuman.add_row([i+1, nama_minuman[i], harga_minuman[i]])
-                i = i + 1
+    harga_minuman = [7000, 
+                    12000, 
+                    7500, 
+                    5000, 
+                    6000, 
+                    8500]
 
-        pesan_minuman = True
-        while pesan_minuman == True:
-                print('\n\tDaftar Minuman')
-                print(tabel_minuman)
-                nomor_minuman = int(input(f'\nSilahkan pilih minuman [input nomor 1 - {jumlah_item}]: '))
-                jawaban_user = str(input('Apakah anda ingin memesan minuman lagi [y/t]: '))
-                minuman_user.append(nomor_minuman-1)
-                if jawaban_user == 'y':
-                        pesan_minuman = True
-                else:
-                        pesan_minuman = False
+    daftar_minuman = buat_tabel()
+    i = 0
+    jumlah_minuman = len(nama_minuman)
+    while i < jumlah_minuman:
+        daftar_minuman.add_row([i+1, nama_minuman[i], harga_minuman[i]])
+        i = i +1
+    
+    minuman_user = []
+    pesan_minuman = True
+    while pesan_minuman == True:
+        print('\n\tDaftar Minuman')
+        print(daftar_minuman)
+        nomor_minuman = int(input(f'\nPilih minuman [1-{jumlah_minuman}]: '))
+        minuman_user.append(nomor_minuman-1)
+        pilihan = str(input('Lagi ? [y/t] : '))
+        if pilihan.lower() == 'y':
+            pesan_minuman = True
+        else:
+            pesan_minuman = False
 
-        total_pembelian = hitung_total_pembelian(minuman_user, harga_minuman)
-        print('total pembelian =', total_pembelian)
-        total_uang = int(input('Silahkan masukan uang anda: '))
-        uang_kembalian = hitung_uang_kembalian(total_uang)
-        print('uang kembalian =', uang_kembalian)
-        keterangan_pembelian(minuman_user, )
+    tampil_keterangan_pesanan(minuman_user, nama_minuman, harga_minuman)
+    jumlah_pesanan = len(minuman_user)
+    total_harga = hitung_total_harga(minuman_user, harga_minuman)
+    print('\nJumlah Pesanan \t:', jumlah_pesanan)
+    print('Total Harga \t:', total_harga)
 
-nama_minuman = ['Sprite', 
-                'Coca-cola', 
-                'Pocari Sweat',
-                'Mizone',
-                'Teh Botol Sosro']
+    jumlah_uang = int(input('\nMasukan uang \t: '))
+    if jumlah_uang >= total_harga:
+        uang_kembalian = hitung_uang_kembalian(jumlah_uang, total_harga)
+        tampil_keterangan_pesanan(minuman_user, nama_minuman, harga_minuman)
+        print('\nJumlah Pesanan \t:', jumlah_pesanan)
+        print('Total Harga \t:', total_harga)
+        print('Jumlah Uang \t:', jumlah_uang)
+        print('Kembalian \t:', uang_kembalian, '\n')
+    else:
+        print('Maaf, uang anda tidak cukup...\n')
 
-harga_minuman = [7000, 
-                 8500, 
-                 5500, 
-                 7500, 
-                 6000]
+def buat_tabel():
+    tabel = PrettyTable()
+    tabel.field_names = ['No', 'Nama Minuman', 'Harga']
+    tabel.align['No'] = 'c'
+    tabel.align['Nama Minuman'] = 'l'
+    tabel.align['Harga'] = 'r'
 
-minuman_user = []
+    return tabel
+
+def tampil_keterangan_pesanan(minuman_user, nama_minuman, harga_minuman):
+    keterangan_pesanan = buat_tabel()
+
+    jumlah_pesanan = len(minuman_user)
+    i = 0
+    while i < jumlah_pesanan:
+        n = minuman_user[i]
+        keterangan_pesanan.add_row([i+1, nama_minuman[n], harga_minuman[n]])
+        i = i +1        
+    
+    print('\n     Keterangan Pesanan')    
+    print(keterangan_pesanan)
+
+def hitung_total_harga(minuman_user, harga_minuman):
+    i = 0
+    total_harga = 0
+    jumlah_pesanan = len(minuman_user)
+    while i < jumlah_pesanan:
+        n = minuman_user[i]
+        total_harga = total_harga + harga_minuman[n]
+        i = i + 1
+    
+    return total_harga
+
+def hitung_uang_kembalian(jumlah_uang, total_harga):
+    uang_kembalian = jumlah_uang - total_harga
+    return uang_kembalian
 
 main()
