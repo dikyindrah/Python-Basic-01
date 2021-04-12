9f84c36d70860838027feef0b9e606bd463ae57c
dikyindrah <dikyindrah@gmail.com>
2021-04-12 21:29:12 +0700

    Update 78-Operasi Matematika Pada Set dan Dictionary

--- 78-Operasi Matematika Pada Set dan Dictionary/Script.py
+++ 78-Operasi Matematika Pada Set dan Dictionary/Script.py
@@ -1,10 +1,12 @@
 # Operasi matematika pada set dan dictionary
 
+print('\n==========Operasi Matematika Pada Set dan Dictionary==========')
+
 dictionary_integerA = {'a':10, 'b':20, 'c':30, 'd':40, 'e':50}
 dictionary_integerB = {'f':60, 'g':70, 'h':80, 'i':90, 'j':100}
 dictionary_integerC = {}
 
-print('\n==========Menghitung Value Pada Satu Dictionary==========')
+print('\n==Menghitung Value Pada Satu Dictionary=====')
 tmp = 0
 for value in dictionary_integerA.values():
     tmp = tmp + value
@@ -15,7 +17,7 @@ for value in dictionary_integerB.values():
     tmp = tmp + value
 print('total nilai:',tmp)
 
-print('\n==========Menghitung Value Pada Dua Dictionary==========')
+print('\n==Menghitung Value Pada Dua Dictionary=====')
 list_keyA = list(dictionary_integerA.keys())
 list_keyB = list(dictionary_integerB.keys())
 length = int((len(dictionary_integerA) + len(dictionary_integerB)) / 2)
