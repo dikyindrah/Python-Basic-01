f78aad8703ed10717c893ce64a5c0e0b678f2e30
dikyindrah <dikyindrah@gmail.com>
2021-05-24 23:11:15 +0700

    Add 86-Menggabungkan String

---
+++ 86-Menggabungkan String/Script.py
@@ -0,0 +1,15 @@
+# Menggabungkan string
+
+print('=====Menggabungkan String=====')
+
+# Cara pertama
+teks1 = 'Saya sedang'
+teks2 = ' belajar python'
+print(teks1+teks2)
+
+# Cara kedua
+print('Selamat'+' malam')
+
+# Cara ketiga
+teks3 = 'Ini adalah' + ' hari senin'
+print(teks3)
