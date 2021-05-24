8984370af04d45dca9de48a2df340502cea718fa
dikyindrah <dikyindrah@gmail.com>
2021-05-24 10:33:12 +0700

    Add 83-String

---
+++ 83-String/Script.py
@@ -0,0 +1,28 @@
+# String
+print('=====Sring=====\n')
+
+print('==Membuat String=====\n')
+# Tanda kutip tunggal
+nama = 'Diky Indra Hermawanto'
+print(nama)
+
+pesan = '''
+            Saya
+                Sedang
+                      Belajar
+                             Python
+        '''
+print(pesan)
+
+# Tanda kutip ganda
+nama = "Ucup Surucup"
+print(nama)
+
+pesan = """
+            Saya
+                Seorang
+                      Programmer
+                                Handal
+        """
+print(pesan)
+
