db1a35186571a1aeee361320bcfc51a656456c1a
dikyindrah <dikyindrah@gmail.com>
2021-05-24 23:12:11 +0700

    Add 88-Memeriksa Suatu Karakter Pada String

---
+++ 88-Memeriksa Suatu Karakter Pada String/Script.py
@@ -0,0 +1,13 @@
+# Memeriksa suatu karakter pada string
+print('=====Memeriksa Suatu Karakter Pada String=====')
+teks = 'python'
+print('teks =',teks)
+
+# Memeriksa apakah ada karakter x pada string
+print('p' in teks)
+print('h' in teks)
+print('k' in teks)
+
+# Memeriksa apakah tidak ada karakter x pada string
+print('k' not in teks)
+print('y' not in teks)
