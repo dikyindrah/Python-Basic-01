1b6f684dd855a2cd7500c732715401433a84ca82
dikyindrah <dikyindrah@gmail.com>
2021-05-26 10:08:20 +0700

    Add 89-Karakter Escape

---
+++ 89-Karakter Escape/Script.py
@@ -0,0 +1,18 @@
+# Karakter escape
+
+print('=====Karakter Escape=====')
+
+# Menampilkan karakter ilegal dengan tanda kutip tiga
+print('''Dia berkata, "Ada apa disana?"''')
+
+# Menampilkan karakter ilegal dengan karakter escape
+print('Dia berkata, \"Ada apa disana?\"')
+
+# Baris baru
+print('Saya sedang\nbelajar python')
+
+# Tab horizontal
+print('Saya sedang\tbelajar python')
+
+# Tab vertikal
+print('Saya sedang\vbelajar python')
