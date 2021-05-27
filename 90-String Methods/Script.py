7e65ab2c1304d5ec80cb31433a3580ee506d0a18
dikyindrah <dikyindrah@gmail.com>
2021-05-27 16:36:29 +0700

    Add 90-String Methods

---
+++ 90-String Methods/Script.py
@@ -0,0 +1,25 @@
+# String methods
+print('=====String Methods=====')
+# Menghubah string menjadi huruf besar
+teks1 = 'ini adalah hari kamis'
+print(teks1.upper())
+
+# Menghubah string menjadi huruf kecil
+teks2 = 'SAYA SEDANG BELAJAR PYTHON'
+print(teks2.lower())
+
+# Memeriksa apakah semua karakter pada string adalah alphabet
+teks3 = 'python'
+teks4 = 'python2'
+teks5 = '12345'
+print(teks3.isalpha())
+print(teks4.isalpha())
+print(teks5.isalpha())
+
+# Memeriksa apakah semua karakter pada string adalah digit angka
+teks6 = '12345'
+teks7 = 'python'
+teks8 = 'python3'
+print(teks6.isdigit())
+print(teks7.isdigit())
+print(teks8.isdigit())
